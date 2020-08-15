cac76e5262df6102a413cb8e67aca82d89415ce4
Natalia Wit <natwit95@gmail.com>
2020-08-15 00:00:31 -0400

    Javascript Added

---
+++ dist/js/main.js
@@ -0,0 +1,23 @@
+//Select DOM Items
+
+//MENU BUTTON ETC>>
+
+const menuBtn = document.querySelector('.btn-menu')
+const menu = document.querySelector('.menu')
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding')
+const navItems = document.querySelectorAll('.nav-items')
+
+//Setting Initial State of Menu 
+
+let showMenu = false
+
+menuBtn.addEventListener('click'.toggleMenu)
+
+function toggleMenu() {
+    if (!showMenu) {
+        
+    } else {
+        
+    }
+}
