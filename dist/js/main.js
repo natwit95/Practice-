82d216cfa5e77f9742dfb1e35e0d687768ca2a7e
Natalia Wit <natwit95@gmail.com>
2020-10-07 00:00:12 -0400

    JS ON BUTTONS DONE

--- dist/js/main.js
+++ dist/js/main.js
@@ -38,3 +38,16 @@ function toggleMenu() {
 
 // var infoBtn = document.getElementsByClassName("collapsible");
 
+let i;
+
+for (i = 0; i < infoBtn.length; i++) {
+	infoBtn[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		let content = this.nextElementSibling;
+		if (content.style.display === "block") {
+			content.style.display = "none";
+		} else {
+			content.style.display = "block";
+		}
+	});
+}
