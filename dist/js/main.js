2dc00c0ea86dd33f869b1fac549f9c8c11b1ae30
Natalia Wit <natwit95@gmail.com>
2020-10-06 23:58:58 -0400

    buttons for work page done

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,6 +7,7 @@ const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
+const infoBtn = document.querySelectorAll(".show-more");
 
 //Setting Initial State of Menu 
 
@@ -34,3 +35,6 @@ function toggleMenu() {
         showMenu = false
     }
 }
+
+// var infoBtn = document.getElementsByClassName("collapsible");
+
